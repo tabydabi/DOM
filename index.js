771a0ab689946da92a21c32fa334dcf9c3755416
Kirill Borun <kbroun@yandex.by>
2020-09-02 22:09:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict'
+let but = document.querySelector('#start');
+console.log(but);
+
+let plus = document.getElementsByTagName('button')[0];
+let plus2 = document.getElementsByTagName('button')[1];
+console.log(plus);
+console.log(plus2);
+
+let checkBox = document.querySelectorAll('#deposit-check');
+console.log(checkBox);
+
+let incomeMoney = document.querySelectorAll('.additional_income-item');
+console.log(incomeMoney);
+
+let minusValue = document.querySelectorAll('.result-total');
+console.log(minusValue);
+
+let salary = document.querySelectorAll('.salary-amount');
+console.log(salary);
+
+let incomeTitle = document.querySelectorAll('.income-title')[1];
+console.log(incomeTitle);
+
+let incomeMoneyAmount = document.querySelectorAll('.income-amount')[0];
+console.log(incomeMoneyAmount);
+
+let expensesTitle = document.querySelectorAll('.expenses-title')[1];
+console.log(expensesTitle);
+
+let expensesAmount = document.querySelectorAll('.expenses-amount')[0];
+console.log(expensesAmount);
+
+let additionalExpenses = document.querySelectorAll('.additional_expenses-item')[0];
+console.log(additionalExpenses);
+
+let target = document.querySelectorAll('.target-amount');
+console.log(target);
+
+let range = document.querySelector('.period-select');
+console.log(range);
+
+
+
+
+
+
